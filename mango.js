6427c4612ff508986918a7898be367393e8f0f2e
Arushi2006 <72396009+Arushi2006@users.noreply.github.com>
2020-11-04 07:23:31 -0800

    Update mango.js

--- mango.js
+++ mango.js
@@ -4,14 +4,19 @@ class Mango
 {
     var options=
     {
-        isStatic=false,
+        isStatic:false,
         restitution:0,
         friction:1
     }
     this.mango=Bodies.circle(x,y,radius,radius,options);
     this.radius=radius;
-    this.image=loadImage("mango.png");
+    this.image=loadImage("FRUITS/mango.png");
     World.add(world,this.mango);
 
 }
+display()
+{
+  image(this.image,this.body.mango.position.x,this.body.mango.position.y,200,400);
 }
+}
+
